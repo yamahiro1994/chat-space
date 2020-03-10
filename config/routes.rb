b7d5d9ad736cf6a9e672fff43b44b5a829cee5c6
yamahiro1994 <y3h5.ja@gmail.com>
2020-03-10 23:08:10 +0900

    hh

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'group#index'
+  root 'groups#index'
+  
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update]
+  resources :groups, only: [:index, :new, :create, :show, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
